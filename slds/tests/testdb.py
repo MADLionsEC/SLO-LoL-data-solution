852e20844a733bc431b6bc402148f05563cc9d03
David Heras <davidherasp@gmail.com>
2018-08-10 16:43:49 +0200

    Updated execution of tests to iterate over the three environments ant test them all

--- slds/tests/testdb.py
+++ slds/tests/testdb.py
@@ -3,10 +3,10 @@ import unittest
 from connectors import database
 import pandas as pd
 from converters.data2frames import get_soloq_dataframe, get_league_dataframe
-from config.constants import API_KEY, SOLOQ, REGIONS, SUPPORTED_LEAGUES
+from config.constants import API_KEY, SOLOQ, REGIONS, SUPPORTED_LEAGUES, SLO, SCRIMS
 
 
-def parse_args():
+def parse_args(league_params):
     parser = argparse.ArgumentParser(description='LoL solution to generate datasets from leagues, scrims and Solo Q'
                                                  ' matches.')
     # Groups
@@ -47,58 +47,63 @@ def parse_args():
     databases.add_argument('-S', '--season', help='Select the season [int]. {leagues data export only]')
     databases.add_argument('-R', '--region_filter', help='Select the region to download and export the data.')
 
-    return parser.parse_args('-c db -l soloq -r euw -d -e -ta MAD -bt 1-3-2018 -et 2-3-2018'.split(' '))
+    return parser.parse_args(league_params.split(' '))
 
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
-        args = parse_args()
-        kwargs = vars(args)
-        region = REGIONS[args.region.upper()]
-        league = args.league.upper()
-        db = database.DataBase(API_KEY, region, league)
-        try:
-            if args.update_static_data:
-                db.save_static_data_files()
-                print('Static data updated.')
+        self.dict1 = {SOLOQ: {'params': '-c db -l {} -r euw -d -e -ta MAD -bt 1-3-2018 -et 2-3-2018'.format(SOLOQ)},
+                      SLO: {'params': '-c db -l {} -r euw -d -e -s spring -S 8'.format(SLO)},
+                      SCRIMS: {'params': '-c db -l {} -r euw -d -e'.format(SCRIMS)}}
+        for key, value in self.dict1.items():
+            args = parse_args(value['params'])
+            kwargs = vars(args)
+            region = REGIONS[args.region.upper()]
+            league = args.league.upper()
+            db = database.DataBase(API_KEY, region, league)
+            try:
+                if args.update_static_data:
+                    db.save_static_data_files()
+                    print('Static data updated.')
 
-            if args.download:
-                print('Downloading.')
-                self.current_game_ids, self.new_game_ids = db.get_old_and_new_game_ids(**kwargs)
-                db.download_games(current_game_ids=self.current_game_ids, new_game_ids=self.new_game_ids)
-                print("\tGames downloaded.")
+                if args.download:
+                    print('Downloading.')
+                    self.dict1[key]['current'], self.dict1[key]['new'] = db.get_old_and_new_game_ids(**kwargs)
+                    db.download_games(current_game_ids=self.dict1[key]['current'], new_game_ids=self.dict1[key]['new'])
+                    print("\tGames downloaded.")
 
-            if args.export:
-                print('Exporting.')
-                self.stored_game_ids = db.get_stored_game_ids(**kwargs)
-                print('\t{} games found.'.format(len(self.stored_game_ids)))
-                if league != SOLOQ:
-                    info_df = get_league_dataframe(db.mongo_cnx.slds.get_collection(league.lower()))
-                    info_df['gid_realm'] = info_df.apply(lambda x: str(x['game_id']) + '_' + str(x['realm']), axis=1)
-                    ls1 = [str(g[0]) + '_' + str(g[1]) for g in self.stored_game_ids]
-                    df = info_df.loc[info_df['gid_realm'].isin(ls1)]
-                else:
-                    df = pd.DataFrame(self.stored_game_ids).rename(columns={0: 'game_id', 1: 'realm'})
-                concatenated_df = db.concat_games(df)
-                final_df = concatenated_df
+                if args.export:
+                    print('Exporting.')
+                    self.dict1[key]['stored'] = db.get_stored_game_ids(**kwargs)
+                    print('\t{} games found.'.format(len(self.dict1[key]['stored'])))
+                    if league != SOLOQ:
+                        info_df = get_league_dataframe(db.mongo_cnx.slds.get_collection(league.lower()))
+                        info_df['gid_realm'] = info_df.apply(lambda x: str(x['game_id']) + '_' + str(x['realm']), axis=1)
+                        ls1 = [str(g[0]) + '_' + str(g[1]) for g in self.dict1[key]['stored']]
+                        df = info_df.loc[info_df['gid_realm'].isin(ls1)]
+                    else:
+                        df = pd.DataFrame(self.dict1[key]['stored']).rename(columns={0: 'game_id', 1: 'realm'})
+                    concatenated_df = db.concat_games(df)
+                    final_df = concatenated_df
 
-                # Merge Solo Q players info with data
-                if league == SOLOQ:
-                    player_info_df = get_soloq_dataframe(db.mongo_players)
-                    self.final_df = final_df.merge(player_info_df, left_on='currentAccountId', right_on='account_id',
-                                            how='left')
+                    # Merge Solo Q players info with data
+                    if league == SOLOQ:
+                        player_info_df = get_soloq_dataframe(db.mongo_players)
+                        self.final_df = final_df.merge(player_info_df, left_on='currentAccountId', right_on='account_id',
+                                                how='left')
 
-                print('\tGames exported.')
-        finally:
-            db.close_connections()
+                    print('\tGames exported.')
+            finally:
+                db.close_connections()
     
     def tearDown(self):
         pass
     
     def test_not_none(self):
-        self.assertIsNotNone(self.current_game_ids)
-        self.assertIsNotNone(self.new_game_ids)
-        self.assertIsNotNone(self.stored_game_ids)
+        for key, value in self.dict1.items():
+            self.assertIsNotNone(self.dict1[key]['current'])
+            self.assertIsNotNone(self.dict1[key]['new'])
+            self.assertIsNotNone(self.dict1[key]['stored'])
         self.assertIsNotNone(self.final_df)
 
 
