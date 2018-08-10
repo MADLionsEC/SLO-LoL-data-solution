4c5e50d990710272fe9a2d892fc4ae1b0ddaaae7
Carlosvioquem <40334887+Carlosvioquem@users.noreply.github.com>
2018-08-10 12:39:53 +0200

    Add files via upload
    Added SoloQ tests.

---
+++ slds/testdb.py
@@ -0,0 +1,118 @@
+import argparse
+import unittest
+from connectors import database
+import pandas as pd
+from converters.data2frames import get_soloq_dataframe, get_league_dataframe
+from config.constants import API_KEY, SOLOQ, REGIONS, SUPPORTED_LEAGUES
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='LoL solution to generate datasets from leagues, scrims and Solo Q'
+                                                 ' matches.')
+    # Groups
+    mandatory = parser.add_argument_group('Mandatory', 'Mandatory commands to run the program.')
+    shared = parser.add_argument_group('Common', 'Shared commands for all systems.')
+    filesystem = parser.add_argument_group('File system', 'Commands used for the file system.')
+    databases = parser.add_argument_group('Databases', 'Commands used for the databases system.')
+
+    # Mandatory commands
+    mandatory.add_argument('-l', '--league', help='Choose league. {}'.format(SUPPORTED_LEAGUES))
+    mandatory.add_argument('-r', '--region', help='Choose region. {}'.format(list(REGIONS.keys())))
+    mandatory.add_argument('-c', '--connector', help='Choose between Databases (DB) or File System (FS) connectors. {}')
+
+    # Shared commands
+    shared.add_argument('-e', '--export', help='Export data.', action='store_true')
+    shared.add_argument('-d', '--download', help='Download new data if available.', action='store_true')
+    shared.add_argument('-usd', '--update_static_data', help='Update static data information.', action='store_true')
+    shared.add_argument('-ng', '--n_games', help='Set the number of games to download from Solo Q.', type=int)
+    shared.add_argument('-bi', '--begin_index', help='Set the begin index of the Solo Q downloads.', type=int)
+    shared.add_argument('-ms', '--merge_soloq', help='Merge SoloQ data with info of players.', action='store_true')
+
+    # FS commands
+    filesystem.add_argument('-xlsx', help='Export data as XLSX.', action='store_true')
+    filesystem.add_argument('-csv', help='Export data as CSV.', action='store_true')
+    filesystem.add_argument('-fu', '--force_update', help='Force the update of the exports datasets.',
+                                   action='store_true')
+
+    # DB commands
+    databases.add_argument('-ta', '--team_abbv', help='Work with the data of one or more teams selected through '
+                                                      'his abbreviation. {download and export}')
+    databases.add_argument('-bt', '--begin_time', help='Set the start date limit of the export (day-month-year). '
+                                                       '{download and export}')
+    databases.add_argument('-et', '--end_time', help='Set the end date limit of the export (day-month-year). '
+                                                     '{download and export}')
+    databases.add_argument('-p', '--patch', help='Select the patch. {export}')
+    databases.add_argument('-C', '--competition', help='Select the competition. {download and export}')
+    databases.add_argument('-s', '--split', help='Select the split [spring, summer]. {leagues data export only]')
+    databases.add_argument('-S', '--season', help='Select the season [int]. {leagues data export only]')
+    databases.add_argument('-R', '--region_filter', help='Select the region to download and export the data.')
+
+    return parser.parse_args('-c db -l soloq -r euw -d -e -ta MAD -bt 1-3-2018 -et 2-3-2018'.split(' '))
+
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        args = parse_args()
+        kwargs = vars(args)
+        region = REGIONS[args.region.upper()]
+        league = args.league.upper()
+        db = database.DataBase(API_KEY, region, league)
+        try:
+            if args.update_static_data:
+                db.save_static_data_files()
+                print('Static data updated.')
+
+            if args.download:
+                print('Downloading.')
+                self.current_game_ids, self.new_game_ids = db.get_old_and_new_game_ids(**kwargs)
+                db.download_games(current_game_ids=self.current_game_ids, new_game_ids=self.new_game_ids)
+                print("\tGames downloaded.")
+
+            if args.export:
+                print('Exporting.')
+                self.stored_game_ids = db.get_stored_game_ids(**kwargs)
+                print('\t{} games found.'.format(len(self.stored_game_ids)))
+                if league != SOLOQ:
+                    info_df = get_league_dataframe(db.mongo_cnx.slds.get_collection(league.lower()))
+                    info_df['gid_realm'] = info_df.apply(lambda x: str(x['game_id']) + '_' + str(x['realm']), axis=1)
+                    ls1 = [str(g[0]) + '_' + str(g[1]) for g in self.stored_game_ids]
+                    df = info_df.loc[info_df['gid_realm'].isin(ls1)]
+                else:
+                    df = pd.DataFrame(self.stored_game_ids).rename(columns={0: 'game_id', 1: 'realm'})
+                concatenated_df = db.concat_games(df)
+                final_df = concatenated_df
+                
+                self.row , self.col = final_df.shape
+                print(final_df.kills.max())
+                print(final_df.kills.min())
+
+                # Merge Solo Q players info with data
+                if league == SOLOQ:
+                    player_info_df = get_soloq_dataframe(db.mongo_players)
+                    self.final_df = final_df.merge(player_info_df, left_on='currentAccountId', right_on='account_id',
+                                            how='left')
+
+                print('\tGames exported.')
+        finally:
+            db.close_connections()
+    
+    def tearDown(self):
+        pass
+    
+    def test_not_none(self):
+        self.assertIsNotNone(self.current_game_ids)
+        self.assertIsNotNone(self.new_game_ids)
+        self.assertIsNotNone(self.stored_game_ids)
+        self.assertIsNotNone(self.final_df)
+    def test_df_size(self):
+        self.assertEqual(self.row, 40, 'Incorrect ROW size.')
+        self.assertEqual(self.col, 222, 'Incorrect COL size.')
+    def test_random_values(self):
+        self.assertEqual(self.final_df.gameId.max(), 3550276067, 'DataFrame content error (max gameId)')
+        self.assertEqual(self.final_df.gameId.min(), 3549167260, 'DataFrame content error (min gameId)')
+        self.assertEqual(self.final_df.kills.max(), 17, 'DataFrame content error (max kills)')
+        self.assertEqual(self.final_df.kills.min(), 0, 'DataFrame content error (min kills)')
+
+
+if __name__ == '__main__':
+    unittest.main()
